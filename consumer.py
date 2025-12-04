ffda8e05d8b216bb2643a1e36936d5c8a9a712d6
Nicolas Franceschini <91314726+Teccybeck@users.noreply.github.com>
2025-12-04 16:34:05 +0100

    Add files via upload

--- consumer.py
+++ consumer.py
@@ -1,60 +1,87 @@
-from confluent_kafka import Producer, Consumer
+import json
+import time
+import random
+import os
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.types import StructType, StructField, StringType, FloatType, TimestampType
 
-def get_current_directory():
-  import os
-  cwd = os.getcwd()
-  print(f"Current working directory: {cwd}")
-  return cwd
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.5.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 pyspark-shell'
 
 def read_config():
-  # reads the client configuration from client.properties
-  # and returns it as a key-value map
-  config = {}
-  with open("../working.properties") as fh:
-    for line in fh:
-      line = line.strip()
-      if len(line) != 0 and line[0] != "#":
-        parameter, value = line.strip().split('=', 1)
-        config[parameter] = value.strip()
-  return config
+    config = {}
+    with open("./working.properties") as fh:
+        for line in fh:
+            line = line.strip()
+            if len(line) != 0 and line[0] != "#":
+                parameter, value = line.strip().split('=', 1)
+                config[parameter] = value.strip()
+    return config
 
+if __name__ == "__main__":
+    
+    spark = SparkSession\
+            .builder\
+            .appName("TD5")\
+            .getOrCreate()
 
-def consume(topic, config):
-  # sets the consumer group ID and offset  
-  config["group.id"] = "python-group-1"
-  config["auto.offset.reset"] = "earliest"
+    sensor_schema = StructType([
+        StructField("sensor_id", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("temperature", FloatType(), True),
+        StructField("humidity", FloatType(), True)
+    ])
 
-  # creates a new consumer instance
-  consumer = Consumer(config)
+    config = read_config()
+    bootstrap_servers = config.get("bootstrap.servers")
+    sasl_username = config.get("sasl.username")
+    sasl_password = config.get("sasl.password")
+    jaas_config = f'org.apache.kafka.common.security.plain.PlainLoginModule required username=\"{sasl_username}\" password=\"{sasl_password}\";'
+    
+    spark.conf.set("spark.sql.streaming.statefulOperator.checkCorrectness.enabled", "false")
 
-  # subscribes to the specified topic
-  # Consumer subscribes to a list of topics, so we pass a list with one element
-  
-  # -- Write your code here using subscribe function ...
-  consumer.subscribe([topic])
+    kafka_options = {
+        "kafka.bootstrap.servers": bootstrap_servers,
+        "kafka.sasl.mechanism": "PLAIN",
+        "kafka.security.protocol": "SASL_SSL",
+        "kafka.sasl.jaas.config": jaas_config,
+        "startingOffsets": "earliest",
+        "auto.offset.reset": "earliest"
+    }
 
-  try:
-    while True:
-      # consumer polls the topic and prints any incoming messages
-      msg = consumer.poll(1.0)
-      if msg is not None and msg.error() is None:
-        key = msg.key().decode("utf-8")
-        value = msg.value().decode("utf-8")
-        print(f"Consumed message from topic {topic}: key = {key:12} value = {value:12}")
-  except KeyboardInterrupt:
-    pass
-  finally:
-    # closes the consumer connection
-    consumer.close()
+    sensors_loaded_stream = spark.readStream\
+        .format("kafka")\
+        .option("subscribe", "IOT_sensor")\
+        .options(**kafka_options)\
+        .load()
+    
+    sensors_stream = sensors_loaded_stream.select(
+        F.col("value").cast(StringType()).alias("json_value")
+    ).select(
+        F.from_json(F.col("json_value"), sensor_schema).alias("data")
+    ).select(F.col("data.*"))
 
-def main():
-  
-  get_current_directory()
+    sensors_state = sensors_stream.groupBy("sensor_id").agg(
+        F.max("timestamp").alias("timestamp"),
+        F.last("temperature").alias("temperature"),
+        F.last("humidity").alias("humidity")
+    )
 
-  config = read_config()
-  topic = "topic_0"
+    sensors_state.createOrReplaceTempView("sensor_table")
 
-  consume(topic, config)
+    querry = """
+    SELECT * FROM sensor_table
+    """
 
+    query_stream = spark.sql(querry).writeStream\
+        .format("console")\
+        .outputMode("complete")\
+        .queryName("name")\
+        .option("truncate", "false")\
+        .trigger(processingTime="5 seconds")\
+        .start()
 
-main()
+    try:
+        query_stream.awaitTermination()
+    except KeyboardInterrupt:
+        print("Arrêt du streaming demandé.")
